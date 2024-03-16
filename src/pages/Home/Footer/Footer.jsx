4d233dc001a679b9ac73197eabc70fd3e45b15fa
Imran Hossain <faraazhossainimran@gmail.com>
2024-03-17 04:14:23 +0600

    footer color improved

--- src/pages/Home/Footer/Footer.jsx
+++ src/pages/Home/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-4 border-t-2 border-[a6c7e3] text-base-content">
+      <footer className="footer footer-center p-4 border-t-2 border-gray-800 text-base-content">
         <aside>
-          <p className="text-white">Copyright © 2023 - All right reserved by Imran Hosasin</p>
+          <p className="text-slate-300">Copyright © 2023 - All right reserved by Imran Hosasin</p>
         </aside>
       </footer>
     </div>
